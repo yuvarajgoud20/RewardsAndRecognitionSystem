e94e04b21d09efcde272b0cadf42d7ccb75ec83c
M YUVARAJ GOUD <myuvaraj.goud@zelis.com>
2025-07-21 19:36:30 +0530

    BannerImage in Email

--- RewardsAndRecognitionRepository/Service/EmailService.cs
+++ RewardsAndRecognitionRepository/Service/EmailService.cs
@@ -7,36 +7,59 @@ using RewardsAndRecognitionRepository.Service;
 public class EmailService : IEmailService
 {
     private readonly EmailSettings _settings;
+    private readonly string imagePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "images", "banner.png");
+
 
     public EmailService(IOptions<EmailSettings> options)
     {
         _settings = options.Value;
     }
 
-    public async Task SendEmailAsync(string subject, string body, string? to = null, string? cc = null, string? bcc = null, List<string>? attachments = null,bool isHtml = false)
+    public async Task SendEmailAsync(
+    string subject, string bodyHtml, string? to = null, string? cc = null, string? bcc = null,
+    List<string>? attachments = null, bool isHtml = false)
     {
         using var message = new MailMessage();
         message.From = new MailAddress(_settings.From);
         message.Subject = subject;
-        message.Body = body;
-        message.IsBodyHtml = isHtml;
 
-        // Add TO
-        //foreach (var email in ParseEmails(to ?? _settings.To))
+        // TO
         message.To.Add(to);
 
-        // Add CC
+        // CC
         if (!string.IsNullOrEmpty(cc ?? _settings.Cc))
         {
             foreach (var email in ParseEmails(cc ?? _settings.Cc))
                 message.CC.Add(email);
         }
 
-        // Add BCC
+        // BCC
         if (!string.IsNullOrEmpty(bcc))
         {
             foreach (var email in ParseEmails(bcc))
                 message.Bcc.Add(email);
+        }
+
+        // HTML with Embedded Image
+        if (isHtml && !string.IsNullOrEmpty(imagePath) && File.Exists(imagePath))
+        {
+            var htmlView = AlternateView.CreateAlternateViewFromString(bodyHtml, null, "text/html");
+
+            var linkedImage = new LinkedResource(imagePath, "image/png")
+            {
+                ContentId = "bannerImage", // should match cid used in HTML
+                TransferEncoding = System.Net.Mime.TransferEncoding.Base64,
+                ContentType = new System.Net.Mime.ContentType("image/png")
+            };
+
+            htmlView.LinkedResources.Add(linkedImage);
+            message.AlternateViews.Add(htmlView);
+            message.IsBodyHtml = true;
+        }
+        else
+        {
+            message.Body = bodyHtml;
+            message.IsBodyHtml = isHtml;
         }
 
         // Attachments
@@ -58,6 +81,7 @@ public class EmailService : IEmailService
 
         await smtpClient.SendMailAsync(message);
     }
+
 
     private List<MailAddress> ParseEmails(string emailList)
     {
