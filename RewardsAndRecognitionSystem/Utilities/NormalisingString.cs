001669bede7279ef525b2126e4cd241ae2c02f74
SAI VIGNESH SULAGANTI <saivignesh.sulaganti@zelis.com>
2025-07-30 12:11:16 +0530

    Spacing,readmore,requiredfields,duplicationcategory

---
+++ RewardsAndRecognitionSystem/Utilities/NormalisingString.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+
+namespace RewardsAndRecognitionSystem.Utilities
+{
+    public class NormalisingString
+    {
+        public static string Normalize(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name)) return string.Empty;
+            name = name.ToLower();
+            name = Regex.Replace(name, @"\baward\b", "");
+            name = Regex.Replace(name, @"[^a-z0-9]", "");
+            return name;
+        }
+    }
+}
