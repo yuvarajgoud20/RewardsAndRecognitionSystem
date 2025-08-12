9ea2d98e9edb60322bf28d4efea5547ee1c42993
ShaikAribaAiman <aribaaimanshaik@gmail.com>
2025-08-12 13:54:33 +0530

    Add files via upload

---
+++ RewardsAndRecognitionSystem/Controllers/CultureController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RewardsAndRecognitionSystem.Controllers
+{
+    public class CultureController : Controller
+    {
+        [HttpPost]
+        public IActionResult SetCulture(string culture, string returnUrl)
+        {
+            if (string.IsNullOrWhiteSpace(culture))
+            {
+                culture = "en-US"; // default to English if empty
+            }
+
+            Response.Cookies.Append(
+                CookieRequestCultureProvider.DefaultCookieName,
+                CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)),
+                new CookieOptions
+                {
+                    Expires = DateTimeOffset.UtcNow.AddYears(1),
+                    IsEssential = true // make sure it's set for GDPR compliance
+                }
+            );
+
+            // Prevent open redirect attacks
+            if (!Url.IsLocalUrl(returnUrl))
+            {
+                returnUrl = Url.Action("Index", "Home")!;
+            }
+
+            return LocalRedirect(returnUrl);
+        }
+    }
+}
